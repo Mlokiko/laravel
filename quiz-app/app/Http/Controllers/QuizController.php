5ab79784e65999b3a531e801af91be7057bf2f2d
WiktorPiechowiak <mlokos225@gmail.com>
2025-12-15 23:15:36 +0100

    Modified QuizController

--- quiz-app/app/Http/Controllers/QuizController.php
+++ quiz-app/app/Http/Controllers/QuizController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
-    //
+    public function index() {
+        return view('quizzes.index');
+    }
+
+    public function show($id) {
+        return view('quizzes.show', ['id' => $id]);
+    }
 }
