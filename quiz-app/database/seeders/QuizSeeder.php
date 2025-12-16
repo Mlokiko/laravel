cb0b7c94c705efd7610dd44c993291385cef5edb
WiktorPiechowiak <mlokos225@gmail.com>
2025-12-17 00:55:08 +0100

    Update seeder

--- quiz-app/database/seeders/QuizSeeder.php
+++ quiz-app/database/seeders/QuizSeeder.php
@@ -38,5 +38,51 @@ class QuizSeeder extends Seeder
         
         $q2->options()->create(['text' => 'app/Http/Controllers', 'is_correct' => false]);
         $q2->options()->create(['text' => 'routes/web.php', 'is_correct' => true]);
+
+        
+        // Quiz 2
+        $quiz2 = Quiz::create([
+            'title' => 'Test 1',
+            'description' => 'test 1 description'
+        ]);
+
+        $q1 = $quiz2->questions()->create([
+            'text' => 'test?'
+        ]);
+
+        $q1->options()->create(['text' => 'test?', 'is_correct' => true]);
+        $q1->options()->create(['text' => 'test!', 'is_correct' => false]);
+        $q1->options()->create(['text' => 'TEST???', 'is_correct' => false]);
+
+        $q2 = $quiz2->questions()->create([
+            'text' => 'test?'
+        ]);
+
+        $q2->options()->create(['text' => 'test?', 'is_correct' => true]);
+        $q2->options()->create(['text' => 'test!', 'is_correct' => false]);
+        $q2->options()->create(['text' => 'TEST???', 'is_correct' => false]);
+
+
+        // Quiz 3
+        $quiz3 = Quiz::create([
+            'title' => 'Best food',
+            'description' => 'test 2 description'
+        ]);
+
+        $q1 = $quiz3->questions()->create([
+            'text' => 'Owoce'
+        ]);
+
+        $q1->options()->create(['text' => 'maliny', 'is_correct' => true]);
+        $q1->options()->create(['text' => 'truskawki', 'is_correct' => false]);
+        $q1->options()->create(['text' => 'czereśnie', 'is_correct' => false]);
+
+        $q2 = $quiz3->questions()->create([
+            'text' => 'obiadowe'
+        ]);
+
+        $q2->options()->create(['text' => 'spaghetti', 'is_correct' => true]);
+        $q2->options()->create(['text' => 'kurczak z rozna', 'is_correct' => false]);
+        $q2->options()->create(['text' => 'pierogi', 'is_correct' => false]);
     }
 }
